49ec49a71bd8fc83f246a6d639f2f388d4ebbf3f
AssafShabili <assaf6063@gmail.com>
2020-05-22 16:00:23 +0300

    add the explain window

---
+++ ProjectsFiles/GUI/GameClient/GameClient-12.01.2020/GameExplaining .xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameClient_12._01._2020
+{
+    /// <summary>
+    /// Interaction logic for GameExplaining.xaml
+    /// </summary>
+    public partial class GameExplaining : Window
+    {
+        public GameExplaining()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+
+        }
+    }
+}
