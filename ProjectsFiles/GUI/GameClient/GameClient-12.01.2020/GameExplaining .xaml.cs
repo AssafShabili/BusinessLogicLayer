57b0978bc13872c9a6c9c3f25edca0e0d1445e00
AssafShabili <assaf6063@gmail.com>
2020-05-22 16:49:31 +0300

    add to the explaing window

--- ProjectsFiles/GUI/GameClient/GameClient-12.01.2020/GameExplaining .xaml.cs
+++ ProjectsFiles/GUI/GameClient/GameClient-12.01.2020/GameExplaining .xaml.cs
@@ -19,9 +19,43 @@ namespace GameClient_12._01._2020
     /// </summary>
     public partial class GameExplaining : Window
     {
+
+        int counter = 0; // הסופר של איזה תמונה לשים בחלון
+        string[] explaining = 
+        {
+            "This is your game board!",
+            "This is the amount of health you have",
+            "This is where you can buy your towers\nif you have enough money to buy it ...\nyou can see you money,Score, and what wave\nyou on",
+            "This is the path\n where your enemies are going to go on",
+            "This is your base where you are going\nto need to defend from enemies\nand BOSSES!",
+            "You can press on your tower to see it's properties",
+            "The tower properties can be seen on\n the (the red arrow) like dmg, attack speed and range\n while (the green arrows) show that you \n can change the tower type and upgrade one of\ntower properties! "
+        };
+
+
         public GameExplaining()
         {
             InitializeComponent();
+
+            ImageExplaning.Source = GetBitmapImage(0);
+            ButtonPre.IsEnabled = false;
+        }
+
+       
+
+        /// <summary>
+        /// פעולה שמקבלת את השם של התמונה   
+        /// ומחזירה את המפה כ - bitmapImage
+        /// </summary>
+        /// <param name="mapName">שם של התמונה של המשחק</param>
+        /// <returns>הפעולה תחזיר את התמונה  שכדאי שנוכל להכניס אותה לתוך הפקד של התמונה</returns>
+        public BitmapImage GetBitmapImage(int index)
+        {
+            BitmapImage bitMap = new BitmapImage();
+            bitMap.BeginInit();
+            bitMap.UriSource = new Uri($@"\Img\ImageExplaning\{index.ToString()}.png", UriKind.Relative);
+            bitMap.EndInit();
+            return bitMap;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
